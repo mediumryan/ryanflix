529c3436a60b41fc303eaa1eced49a29d74ed4bb
ryan <straightwind0726@gmail.com>
2025-04-29 13:27:16 +0900

    [Update] 이미지 로딩 기능 추가

--- src/features/movie/content.tsx
+++ src/features/movie/content.tsx
@@ -8,6 +8,7 @@ import { AccentTextColor } from '@/service/common';
 import { Movie } from '@/service/movieService';
 import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
+import { Loader } from 'lucide-react';
 
 interface ContentProps {
   data: Movie[];
@@ -17,7 +18,9 @@ interface ContentProps {
 export default function Content({ data, type }: ContentProps) {
   const [dataArr, setDataArr] = useState<Movie[]>([]);
   const [page, setPage] = useState(2);
+  const [isLoadedList, setIsLoadedList] = useState<boolean[]>([]);
 
+  // Intersection Observer
   const { ref, inView } = useInView({
     triggerOnce: false,
     threshold: 0.25,
@@ -26,6 +29,7 @@ export default function Content({ data, type }: ContentProps) {
   useEffect(() => {
     if (dataArr.length === 0) {
       setDataArr(data);
+      setIsLoadedList(Array(data.length).fill(false));
     }
   }, [data, dataArr.length]);
 
@@ -36,15 +40,18 @@ export default function Content({ data, type }: ContentProps) {
         : type === 'top-rated'
         ? 'getTopRated'
         : 'getNowPlaying';
+
     const getData = async () => {
       setPage((pre) => pre + 1);
       const res = await fetch(`/movie/api/${reqType}/${page}`);
-      const data = await res.json();
-      setDataArr((pre) => {
-        const newData = [...pre, ...data];
-        return newData;
-      });
+      const newData = await res.json();
+      setDataArr((prev) => [...prev, ...newData]);
+      setIsLoadedList((prev) => [
+        ...prev,
+        ...Array(newData.length).fill(false),
+      ]);
     };
+
     if (inView) {
       getData();
     }
@@ -55,31 +62,46 @@ export default function Content({ data, type }: ContentProps) {
       id={`movie-content-${type}`}
       className="grid grid-cols-2 md:grid-cols-5 gap-4 px-12 mb-12 md:px-24 md:pb-24"
     >
-      {dataArr.length > 0 &&
-        dataArr.map((item: any) => {
-          return (
-            <Card key={item.id}>
-              <CardContent className="group relative flex aspect-square items-center justify-center p-2 origin-bottom duration-300 hover:-translate-y-4">
-                <Link href={`/movie/detail/${item.id}`}>
-                  <Image
-                    fill
-                    className="rounded-md group-hover:opacity-15"
-                    src={getImages(item.poster_path || item.backdrop_path)}
-                    alt={item.title}
-                  />
-                  <div className="absolute text-white top-0 left-0 w-full h-full pt-4 px-4 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100">
-                    <h4
-                      className="text-xl"
+      {dataArr.map((item, index) => (
+        <Card key={item.id}>
+          <CardContent className="group relative flex aspect-square items-center justify-center p-2 origin-bottom duration-300 hover:-translate-y-4">
+            <Link href={`/movie/detail/${item.id}`}>
+              {!isLoadedList[index] && (
+                <>
+                  <div className="absolute inset-0 bg-gray-300 animate-pulse rounded-md z-10"></div>
+                  <div className="w-full h-full flex items-center justify-center">
+                    <Loader
+                      className="animate-spin text-gray-500"
+                      size={40}
                       style={{ textShadow: AccentTextColor }}
-                    >
-                      {item.title}({item?.vote_average?.toFixed(1)})
-                    </h4>
+                    />
                   </div>
-                </Link>
-              </CardContent>
-            </Card>
-          );
-        })}
+                </>
+              )}
+              <Image
+                fill
+                className={`rounded-md group-hover:opacity-15 transition-opacity duration-300 ${
+                  isLoadedList[index] ? 'opacity-100' : 'opacity-0'
+                }`}
+                src={getImages(item.poster_path || item.backdrop_path)}
+                alt={item.title}
+                onLoad={() => {
+                  setIsLoadedList((prev) => {
+                    const newArr = [...prev];
+                    newArr[index] = true;
+                    return newArr;
+                  });
+                }}
+              />
+              <div className="absolute text-white top-0 left-0 w-full h-full pt-4 px-4 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100">
+                <h4 className="text-xl" style={{ textShadow: AccentTextColor }}>
+                  {item.title}({item.vote_average?.toFixed(1)})
+                </h4>
+              </div>
+            </Link>
+          </CardContent>
+        </Card>
+      ))}
       <div ref={ref} className="w-full h-12"></div>
     </div>
   );
--- src/features/tv/content.tsx
+++ src/features/tv/content.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Card, CardContent } from '@/components/ui/card';
 import { AccentTextColor } from '@/service/common';
-import { Movie } from '@/service/movieService';
 import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { Tv } from '@/service/tvShowService';
+import { Loader } from 'lucide-react';
 
 interface ContentProps {
   data: Tv[];
@@ -18,6 +18,7 @@ interface ContentProps {
 export default function Content({ data, type }: ContentProps) {
   const [dataArr, setDataArr] = useState<Tv[]>([]);
   const [page, setPage] = useState(2);
+  const [isLoadedList, setIsLoadedList] = useState<boolean[]>([]);
 
   const { ref, inView } = useInView({
     triggerOnce: false,
@@ -49,35 +50,53 @@ export default function Content({ data, type }: ContentProps) {
     if (inView) {
       getData();
     }
-  }, [inView, page]);
+  }, [inView, page, type]);
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-5 gap-4 px-12 mb-12 md:px-24 md:pb-24">
-      {dataArr.length > 0 &&
-        dataArr.map((item: any) => {
-          return (
-            <Card key={item.id}>
-              <CardContent className="group relative flex aspect-square items-center justify-center p-2 origin-bottom duration-300 hover:-translate-y-4">
-                <Link href={`/movie/detail/${item.id}`}>
-                  <Image
-                    fill
-                    className="rounded-md group-hover:opacity-15"
-                    src={getImages(item.poster_path || item.backdrop_path)}
-                    alt={item.name}
-                  />
-                  <div className="absolute text-white top-0 left-0 w-full h-full pt-4 px-4 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100">
-                    <h4
-                      className="text-xl"
+    <div
+      id={`tv-content-${type}`}
+      className="grid grid-cols-2 md:grid-cols-5 gap-4 px-12 mb-12 md:px-24 md:pb-24"
+    >
+      {dataArr.map((item, index) => (
+        <Card key={item.id}>
+          <CardContent className="group relative flex aspect-square items-center justify-center p-2 origin-bottom duration-300 hover:-translate-y-4">
+            <Link href={`/tv/detail/${item.id}`}>
+              {!isLoadedList[index] && (
+                <>
+                  <div className="absolute inset-0 bg-gray-300 animate-pulse rounded-md z-10"></div>
+                  <div className="w-full h-full flex items-center justify-center">
+                    <Loader
+                      className="animate-spin text-gray-500"
+                      size={40}
                       style={{ textShadow: AccentTextColor }}
-                    >
-                      {item.name}({item?.vote_average?.toFixed(1)})
-                    </h4>
+                    />
                   </div>
-                </Link>
-              </CardContent>
-            </Card>
-          );
-        })}
+                </>
+              )}
+              <Image
+                fill
+                className={`rounded-md group-hover:opacity-15 transition-opacity duration-300 ${
+                  isLoadedList[index] ? 'opacity-100' : 'opacity-0'
+                }`}
+                src={getImages(item.poster_path || item.backdrop_path)}
+                alt={item.name ? item.name : 'No title'}
+                onLoad={() => {
+                  setIsLoadedList((prev) => {
+                    const newArr = [...prev];
+                    newArr[index] = true;
+                    return newArr;
+                  });
+                }}
+              />
+              <div className="absolute text-white top-0 left-0 w-full h-full pt-4 px-4 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100">
+                <h4 className="text-xl" style={{ textShadow: AccentTextColor }}>
+                  {item.title}({item.vote_average?.toFixed(1)})
+                </h4>
+              </div>
+            </Link>
+          </CardContent>
+        </Card>
+      ))}
       <div ref={ref} className="w-full h-12"></div>
     </div>
   );
